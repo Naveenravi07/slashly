f0e00fd5446e2fb7786852d7bc0fbe4167657e2e
Naveenravi07 <gamerkidnav@gmail.com>
2026-03-08 23:34:38 +0530

    Dockerizeed

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -12,7 +12,7 @@ server.get('/ping', async (request, reply) => {
 server.register(prismaPlugin)
 server.register(urlShortenerRoutes)
 
-server.listen({ port: 8080 }, (err, address) => {
+server.listen({ port: 8080, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
